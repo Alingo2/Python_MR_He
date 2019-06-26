ac54b26d4a8c38811770074587009fbf218f7b2c
Alingo2 <762480390@qq.com>
2019-06-26 18:37:28 +0800

    Second commit

--- bilibili_fans_portrait.py
+++ bilibili_fans_portrait.py
@@ -4,35 +4,60 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup as bs
 import xlwt
+import requests
+from http import cookiejar
 
 def get_source():
-    WAIT.until(EC.presence_of_element_located((By.CSS_SELECTOR,'#server-search-app > div.contain > div.body-contain > div > div.result-wrap.clearfix')))
+    WAIT.until(EC.presence_of_element_located((By.CSS_SELECTOR, '#server-search-app > div.contain > div.body-contain > div > div.result-wrap.clearfix')))
     html = browser.page_source
-    soup = BeautifulSoup(html,'lxml')
+    soup = bs(html, 'lxml')
     #save_to_excel(soup)
 
-browser = webdriver.Chrome()        #拿到浏览器的对象
+browser = webdriver.Firefox()        #拿到浏览器的对象
 WAIT = WebDriverWait(browser, 10)   #设置最长超时时间
-browser.set_window_size(1400,900)
+browser.set_window_size(1400, 900)
+header={
+    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:47.0) Gecko/20100101 Firefox/47.0'
+}#Firefox的用户代理
 
-browser.get("https://bilibili.com/")    #访问b站
+def search():
+    try:
+        print('访问b站....')
+        browser.get("https://bilibili.com/")    #访问b站
+        s = requests.Session()
+        s.cookies = cookiejar.CookieJar()
+        site = s.get('https://bilibili.com/', headers=header)
 
-index = WAIT.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "#primary_menu > ul > li.home > a")))
-# 被那个登录遮住了 解决：先去主页刷新一下，再登陆
-index.click()
+        index = WAIT.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "#primary_menu > ul > li.home > a")))
+        # 被那个登录遮住了 解决：先去主页刷新一下，再登陆
+        index.click()
 
-input = WAIT.until(EC.presence_of_element_located((By.CSS_SELECTOR, "#banner_link > div > div > form > input")))
-submit = WAIT.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="banner_link"]/div/div/form/button')))
-#这里等到这个元素可操作的时候才会继续执行下一步获取到输入框元素后输入「何同学」  确保可以正常搜索
+        input = WAIT.until(EC.presence_of_element_located((By.CSS_SELECTOR, "#banner_link > div > div > form > input")))
+        submit = WAIT.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="banner_link"]/div/div/form/button')))
+        #这里等到这个元素可操作的时候才会继续执行下一步获取到输入框元素后输入「何同学」  确保可以正常搜索
 
-input.send_keys('老师好我叫何同学')
-submit.click()
+        input.send_keys('老师好我叫何同学')
+        submit.click()
+        #输入搜索信息，提交
 
+        #跳转到新的窗口
+        print('跳转到新窗口')
+        all_h = browser.window_handles
+        browser.switch_to.window(all_h[1])
+        get_source()
 
-# 跳转到新的窗口
-print('跳转到新窗口')
-all_h = browser.window_handles
-browser.switch_to.window(all_h[1])
-get_source()
+        total=get_pagenum(site)
+        return int(total.text)
+    except TimeoutException:
+        return search()
+
+def get_pagenum(site):
+    soup = bs(site.text, 'html.parser')
+    num = soup.find_all('button', class_='pagination-btn num-btn')
+    print(num)
+    return
+#主函数
+total=search()
+print(total)
